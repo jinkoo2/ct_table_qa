62506edd79180dbe4a40452ed4f98befa86df53d
Jinkoo <jinkoo2@hotmail.com>
2023-07-03 10:03:42 -0400

    first commit

---
+++ ct_table_qa_service/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ctqa_mpc_service
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new ctqs_mpc_service()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
---
+++ ct_table_qa_service/ctqs_mpc_service.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ctqa_mpc_service
+{
+    public partial class ctqs_mpc_service : ServiceBase
+    {
+        public ctqs_mpc_service()
+        {
+            InitializeComponent();
+        }
+        
+        protected override void OnStart(string[] args)
+        {
+            var appSettings = System.Configuration.ConfigurationManager.AppSettings;
+
+            //////////////////////
+            // watcher
+            string service_param_file = appSettings["service_param_file"];
+            ctqa_lib.fswatcher watcher = new ctqa_lib.fswatcher(service_param_file);
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
